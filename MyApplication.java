d78e56a18e8f99ea3e533619259f355f098f3a94
manarbekulybatyrhan-stack <manarbekulybatyrhan@gmail.com>
2025-12-19 19:56:21 +0500

    Update MyApplication.java

--- MyApplication.java
+++ MyApplication.java
@@ -9,25 +9,23 @@ import java.util.Scanner;
 public class MyApplication {
     public static void main(String[] args) throws FileNotFoundException {
 
-        // file where coordinates are stored
+        
         File file = new File("src/source");
         Scanner sc = new Scanner(file);
 
-        // needed for numbers with dot
+        
         sc.useLocale(Locale.US);
-        // split input by comma or space
         sc.useDelimiter("[,\\s]+");
 
         Shape shape = new Shape();
 
-        // read x and y from file
         while (sc.hasNextDouble()) {
             double x = sc.nextDouble();
             double y = sc.nextDouble();
             shape.addPoint(new Point(x, y));
         }
 
-        // print results
+        
         System.out.println(shape.calculatePerimeter());
         System.out.println(shape.getAverageSide());
         System.out.println(shape.getLongest());
